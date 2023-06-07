f09e6d1948a712220ea3d5fc235389ebfeb43f8f
Sudhir Gupta <sudhirnagendragupta@gmail.com>
2023-06-07 19:10:26 -0400

    Secure the booking api

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken', 
+    'djoser',
     'restaurant',
 ]
 
@@ -144,3 +146,32 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER={
+    "USER_ID_FIELD":"username"
+}
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_RENDERER_CLASSES' : [
+    #     'rest_framework.renderers.JSONRenderer',
+    #     'rest_framework.renderers.BrowsableAPIRenderer',
+    # ],
+    # 'DEFAULT_FILTER_BACKENDS': [
+    #     'rest_framework.filters.OrderingFilter',
+    #     'rest_framework.filters.SearchFilter',
+    # ],
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #'PAGE_SIZE' : 5,
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle'
+    # ],
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'anon': '10/minute',
+    #     'user': '100/minute',
+    # }
+}
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -20,7 +21,8 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer    
     
-class BookingViewSet(viewsets.ModelViewSet):    
+class BookingViewSet(viewsets.ModelViewSet):  
+    permission_classes = [IsAuthenticated]  
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     
